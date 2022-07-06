44f617050283d0c6dbdc6cba9ef7367f41332133
Niel Principe <codemagnusniel@gmail.com>
2022-07-06 11:17:04 +0800

    Add build folder

---
+++ build/dist/build/dist/CounterInput.d.ts
@@ -0,0 +1,13 @@
+export var __esModule: boolean;
+declare var _default: typeof CounterInput;
+export default _default;
+declare const CounterInput_base: typeof globalThis.React.Component;
+declare class CounterInput extends CounterInput_base {
+    constructor(props: any);
+    handleOnIncreasePress: () => void;
+    handleOnDecreasePress: () => void;
+    handleOnChangeText: (text: any) => void;
+    renderIncreaseCounter: () => JSX.Element;
+    renderDecreaseCounter: () => JSX.Element;
+    renderTextInput: () => JSX.Element;
+}
---
+++ build/dist/build/dist/CounterInput.style.d.ts
@@ -0,0 +1,68 @@
+export var __esModule: boolean;
+export var _decreaseButtonStyle: ((isPressed: any, decreaseButtonBackgroundColor: any) => {
+    width: number;
+    height: number;
+    borderRadius: number;
+    alignItems: string;
+    justifyContent: string;
+    backgroundColor: any;
+    shadowOpacity: number;
+    shadowRadius: number;
+    shadowColor: string;
+    shadowOffset: {
+        width: number;
+        height: number;
+    };
+}) | undefined;
+export var _increaseButtonStyle: ((isPressed: any, increaseButtonBackgroundColor: any) => {
+    width: number;
+    height: number;
+    borderRadius: number;
+    alignItems: string;
+    justifyContent: string;
+    backgroundColor: any;
+    shadowOpacity: number;
+    shadowRadius: number;
+    shadowColor: string;
+    shadowOffset: {
+        width: number;
+        height: number;
+    };
+}) | undefined;
+export var _container: ((width: any, horizontal: any, backgroundColor: any, borderRadius: any) => {
+    width: any;
+    backgroundColor: any;
+    borderRadius: any;
+    padding: number;
+    minHeight: number;
+    maxWidth: number | undefined;
+    flexDirection: string;
+    alignItems: string;
+    justifyContent: string;
+    shadowRadius: number;
+    shadowOpacity: number;
+    shadowColor: string;
+    elevation: number;
+    shadowOffset: {
+        width: number;
+        height: number;
+    };
+}) | undefined;
+declare namespace _default {
+    export namespace textInputStyle {
+        export const width: number;
+        export const minHeight: number;
+        export const fontSize: number;
+        export const marginTop: number;
+        export const marginBottom: number;
+        export const alignSelf: "center";
+        export const fontWeight: "bold";
+        export const textAlign: "center";
+    }
+    export namespace buttonImageStyle {
+        const width_1: number;
+        export { width_1 as width };
+        export const height: number;
+    }
+}
+export default _default;
